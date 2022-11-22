87dbed49fc36f660f35032ec0a8e59ca0495d67a
Francisco Patricio <fpatricio1@proton.me>
2022-11-22 13:41:56 +0000

    delete almost done

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   #has_one_attached :photo # para o cloudinary
   belongs_to :user
-  has_many :rents
+  has_many :rents, dependent: :destroy
 
   validates :name, :description, :min_rent_duration, :material, :rent_cost, :city, :state, presence: true
 end
