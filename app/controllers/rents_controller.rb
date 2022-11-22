dab8e7311bae5d15c612c4c0f6d91444fc226b99
manuel esteves <mj.esteves92@gmail.com>
2022-11-22 21:44:45 +0000

    destroy action/link

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -1,4 +1,5 @@
 class RentsController < ApplicationController
+  before_action :set_rent, only: %i[new create]
   def index
     @rents = Rent.where(user: current_user)
   end
@@ -11,15 +12,22 @@ class RentsController < ApplicationController
     @rent = Rent.new
   end
 
-  # def create
-  #   @rent = Rent.new(rent_params)
-  #   @rent.user = current_user
-  #   if @rent.save
-  #     redirect_to rents_path(@rent)
-  #   else
-  #     render :new, status: :unprocessable_entity
-  #   end
-  # end
+  def create
+    @rent = Rent.new(rent_params)
+    @rent.user = current_user
+    if @rent.save
+      redirect_to rents_path(@rent)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @rent = Rent.find(params[:id])
+    @rent.destroy
+    redirect_to rents_path, status: :see_other
+  end
+
 
   private
 
