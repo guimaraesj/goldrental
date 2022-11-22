b1de5d40e8c848a82bb88dc19d42efbeef94068d
Francisco Patricio <fpatricio1@proton.me>
2022-11-22 12:14:46 +0000

    new & create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,21 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+    #link to
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.user = current_user
+    if @product.save
+      redirect_to products_path(@product) #ver no rails routes para onde direcionar
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -15,4 +30,7 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def product_params
+    params.require(:product).permit([:name, :description, :min_rent_duration, :weight, :material, :product_price, :rent_cost, :city, :state])
+  end
 end
