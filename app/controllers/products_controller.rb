0be24c57168f4fca718e2629dc558300b296b538
manuel esteves <mj.esteves92@gmail.com>
2022-11-22 13:19:55 +0000

    Edit and Update Actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,29 @@
 class ProductsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[show index]
-
+  before_action :set_product, only: %i[show edit]
   def index
     @products = Product.all
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
     @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to product_path(@product)
   end
 
   private
 
+  def product_params
+    params.require(:product).permit(:name, :description, :min_rent_duration, :material, :rent_cost, :city, :state)
+  end
+
   def set_product
     @product = Product.find(params[:id])
   end
-
 end
