8d21d81b810a61b753ea8aed57e882ddbb8ab59f
Jorge Guimaraes <jdguimaraes@hotmail.com>
2022-11-23 16:22:13 +0000

    Fixed Pundit Validations and Initial HTML

---
+++ app/policies/rent_policy.rb
@@ -0,0 +1,27 @@
+class RentPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      user.admin? ? scope.all : scope.where(user: user)
+    end
+  # scope ends here!
+  end
+
+  def show?
+    record.user == user
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    record.user == user
+    # record: the rents passed to the `authorize` method in controller
+    # user: the `current_user` signed in with Devise
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
